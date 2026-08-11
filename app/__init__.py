6334d27e6566421abd5175b2cfda9214ee5a15aa
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-08-11 12:47:38 +0000

    app setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from auth import router as auth_router
+from app.routes.send_otp import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 from contextlib import asynccontextmanager
-from extension import connect_nats, close_nats
+from app.extension import connect_nats, close_nats
 
 load_dotenv()
+
 
 def create_app():
     @asynccontextmanager
     async def lifespan(app: FastAPI):
-        await connect_nats
+        await connect_nats()
         yield
-        await close_nats
+        await close_nats()
 
     app = FastAPI(lifespan=lifespan)
     app.include_router(auth_router)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
