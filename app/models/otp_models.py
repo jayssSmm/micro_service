11bd4e0abffd838b3fc11db5feb3e44346c9afc9
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-08-12 15:52:10 +0000

    otp models altered

--- app/models/otp_models.py
+++ app/models/otp_models.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
 
+
 class OTPRequest(BaseModel):
     email: str
 
 
 class OTPResponse(BaseModel):
+    message: str
+    email: str
+
+
+class VerifyOTPRequest(BaseModel):
+    email: str
+    otp: str
+
+
+class VerifyOTPResponse(BaseModel):
     message: str
     email: str
